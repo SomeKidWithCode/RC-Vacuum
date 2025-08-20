50784d1387036385502b883debd7422b8141ddaf
SomeKidWithCode <0438786@schoolsnet.act.edu.au>
2025-08-20 16:42:22 +1000

    A quick reshuffle of things. This should also be able to be run as the ino files are in same-name folders.

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,30 +1,34 @@
+// Libs
 #include <SPI.h>
 #include "printf.h"
 #include "RF24.h"
 
+// Transceiver pins
 #define CE_PIN 9
 #define CSN_PIN 10
 
+// Joystick pins
 #define VRX_PIN 5
 #define VRY_PIN 6
 #define SW_PIN 4
 
-
-bool swit = false;
-
+// Defining transceiver things
 RF24 transceiver(CE_PIN, CSN_PIN);
 const byte readingPipe[6] = "000001";
 const byte writingPipe[6] = "000002";
 
+
 void setup() {
+  // Transceiver setup
   transceiver.begin();
   transceiver.openWritingPipe(writingPipe);
   transceiver.setPALevel(RF24_PA_MIN);
-
   transceiver.setPayloadSize(sizeof(char));
 
+  // Begin serial
   Serial.begin(9600);
 
+  // Set pins
   pinMode(VRX_PIN, INPUT);
   pinMode(VRY_PIN, INPUT);
   pinMode(SW_PIN, INPUT);
@@ -47,7 +51,3 @@ void loop() {
   delay(250);
 }
 
-class Test {
-public:
-  static const int a = 0;
-};
---
+++ Robot/Robot.ino
@@ -0,0 +1,96 @@
+// Libs
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+
+// Transceiver pins
+#define CE_PIN 9
+#define CSN_PIN 10
+
+// Motor pins
+#define MOTOR_A_IN_1 5
+#define MOTOR_A_IN_2 4
+#define MOTOR_B_IN_1 3
+#define MOTOR_B_IN_2 2
+
+
+
+
+
+// Abstract away the motor control
+class Motor {
+    int pinA;
+    int pinB;
+    bool reversed = false;
+    
+  public:
+    Motor(int _pinA, int _pinB) {
+      pinA = _pinA;
+      pinB = _pinB;
+    }
+    
+    void begin() {
+      pinMode(pinA, OUTPUT);
+      pinMode(pinB, OUTPUT);
+    }
+    
+    void reverse() {
+      reversed = true;
+    }
+
+    void start() {
+      digitalWrite(pinA, reversed ? LOW : HIGH);
+      digitalWrite(pinA, reversed ? HIGH : LOW);
+    }
+
+    void stop() {
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, LOW);
+    }
+};
+
+
+
+
+
+// Defining transceiver things
+RF24 transceiver(CE_PIN, CSN_PIN);
+const byte readingPipe[6] = "000001";
+const byte writingPipe[6] = "000002";
+
+// Defining motors
+Motor leftMotor(MOTOR_A_IN_1, MOTOR_A_IN_2);
+Motor rightMotor(MOTOR_B_IN_1, MOTOR_B_IN_2);
+
+
+void setup() {
+  // Transceiver setup
+  transceiver.begin();
+  transceiver.openReadingPipe(0, readingPipe);
+  transceiver.setPALevel(RF24_PA_MIN);
+  transceiver.startListening();
+  transceiver.setPayloadSize(sizeof(int));
+
+  // Set up motors
+  leftMotor.begin();
+  rightMotor.begin();
+  leftMotor.reverse();
+}
+
+void loop() {
+  if (transceiver.available()) {
+    char text[32] = "";
+    transceiver.read(&text, sizeof(text));
+    
+  }
+
+  // Start motors
+  leftMotor.start();
+  rightMotor.start();
+  delay(500);
+
+  // Stop motors
+  leftMotor.stop();
+  rightMotor.stop();
+  delay(500);
+}

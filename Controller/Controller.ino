efe8da6c3d71fb3d9eac7105487b761087de12b3
SomeKidWithCode <0438786@schoolsnet.act.edu.au>
2025-10-30 18:28:29 +1100

    Final commit

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -2,179 +2,95 @@
 #include "SPI.h"
 #include "RF24.h"
 #include "printf.h"
-#include "stdarg.h"
 
 
 // Transceiver pins
 const byte CE_PIN = 9;
 const byte CSN_PIN = 10;
 
-// Joystick pins
-const byte VRX_PIN = A0;
-const byte VRY_PIN = A1;
-const byte SW_PIN = 2;
+// Controller pins
+const byte LEFT_POT_PIN = A0;
+const byte RIGHT_POT_PIN = A1;
+const byte VRX_PIN = A2;
+const byte VRY_PIN = A3;
+const byte BTN_PIN = 8;
 
+const int ZeroRange = 25;
 
 struct TransData {
-    int leftMotorSpeed;
-    int rightMotorSpeed;
-    bool reverse;
+    int leftMotorSpeed = 0;
+    int rightMotorSpeed = 0;
     bool tV = false;
 };
-
+const int DataSize = sizeof(TransData);
 
 // Defining transceiver params
 RF24 transceiver(CE_PIN, CSN_PIN);
 const byte readingPipe[6] = "000001";
 const byte writingPipe[6] = "000002";
 
-
-bool isEven(int n) { return (n & 1) == 0; }
-bool isNegative(int n) { return n < 0; }
-bool between(int n, int min, int max) { return n > min && max > n; }
-
-// Magic function for performing magic
-void printn(String types...) {
-    va_list args;
-    int count = sizeof(types) - 1;
-    va_start(args, count);
-
-    for (int i = 0; i < count; i++) {
-        if (types[i] == 's')
-            if (i == count - 1)
-                Serial.println(va_arg(args, String));
-            else
-                Serial.print(va_arg(args, String));
-        else if (types[i] == 'i')
-            if (i == count - 1)
-                Serial.println(va_arg(args, int));
-            else
-                Serial.print(va_arg(args, int));
-        else if (types[i] == 'f')
-            if (i == count - 1)
-                Serial.println(va_arg(args, float));
-            else
-                Serial.print(va_arg(args, float));
-        else if (types[i] == 'b')
-            if (i == count - 1)
-                Serial.println(va_arg(args, bool));
-            else
-                Serial.print(va_arg(args, bool));
-    }
-
-    va_end(args);
-}
-
-
 void setup() {
     // Begin serial
     Serial.begin(9600);
     // And wait for it to be ready
-    while (!Serial) { }
+    while (!Serial) {}
 
     // Transceiver setup
     if (!transceiver.begin()) {
         Serial.println(F("Radio hardware not responding"));
-        while (1) { }  // Hold program in infinite loop until transceiver is ready
+        while (1) {} // Hold program in infinite loop until transceiver is ready
     }
     transceiver.openWritingPipe(writingPipe);
     transceiver.setPALevel(RF24_PA_MIN);
-    //transceiver.setPayloadSize(sizeof(TransData)); // This isn't nessesary, but it potentially makes transmission faster
+    transceiver.setPayloadSize(DataSize); // This isn't nessesary, but it potentially makes transmission faster
 
     // Set pins
     pinMode(VRX_PIN, INPUT);
     pinMode(VRY_PIN, INPUT);
-    pinMode(SW_PIN, INPUT);
+    pinMode(BTN_PIN, INPUT);
 }
 
 void loop() {
-    // Read joystick values
-    // x is left and right (pos-neg)
-    // y is up and down (pos-neg)
-    int x = analogRead(VRX_PIN);
-    int y = analogRead(VRY_PIN);
-    int sw = digitalRead(SW_PIN);
-    
-
-    if (true) {
-        Serial.print("Joystick: x: ");
-        Serial.print(x);
-        Serial.print(", y: ");
-        Serial.print(y);
-        Serial.print(", switch: ");
-        Serial.println(sw);
-        /*printn("sisisi", 
-            "Joystick: x: ",
-            x,
-            ", y: ",
-            y,
-            ", switch: ",
-            sw
-        );*/
-    }
-
-    // Map joystick values
-    int mapped_x = map(x, 0, 1023, -255, 255);
-    int mapped_y = map(y, 0, 1023, -255, 255);
-
-    // Because the standby position is not perfectly 0, we have to add a buffer range
-    if (between(mapped_y, -10, 10))
-      mapped_y = 0;
-    if (between(mapped_x, -10, 10))
-      mapped_x = 0;
-
-
     TransData tData;
 
-    // Step 1, reverse directions if needed
-    tData.reverse = isNegative(mapped_y);
+    // Read joystick values
+    int x = analogRead(VRX_PIN);
+    int y = analogRead(VRY_PIN);
+    int btn = digitalRead(BTN_PIN);
 
-    // Step 2, *magic*
-    // Set the inital motor speeds to the absolute y value
-    // Because this ranges from -255 to 255, we'll get the speed which can only
-    // be 0-255
-    int leftSpeed = abs(mapped_y);
-    int rightSpeed = abs(mapped_y);
+    // Map joystick values
+    x = map(x, 0, 1023, -1, 1);
+    y = map(y, 0, 1023, -1, 1);
 
-    
-    if (isNegative(mapped_x))
-        // += because mapped_x is already -ve
-        rightSpeed += mapped_x;
-    else
-        leftSpeed -= mapped_x;
-
-    tData.leftMotorSpeed = leftSpeed;
-    tData.rightMotorSpeed = rightSpeed;
-
-
-    // Experiment
-    if (Serial.available()) {
-        tData.tV = true;
-        Serial.println("Exp trig");
-        Serial.read();
+    if (x == -1) {
+        tData.leftMotorSpeed = 255;
+        tData.rightMotorSpeed = -255;
+    } else if (x == 1) {
+        tData.leftMotorSpeed = -255;
+        tData.rightMotorSpeed = 255;
     }
 
+    if (y == -1) {
+        tData.leftMotorSpeed = -255;
+        tData.rightMotorSpeed = -255;
+    } else if (y == 1) {
+        tData.leftMotorSpeed = 255;
+        tData.rightMotorSpeed = 255;
+    }
 
-    bool transmitted = transceiver.write(&tData, sizeof(TransData));
+    if (btn) {
+        tData.tV = true;
+        Serial.println("Exp trig");
+        //Serial.read(); // Must read to consume the serial data and prevent it constantly triggering
+    }
+
+    bool transmitted = transceiver.write(&tData, DataSize);
     if (transmitted) {
         Serial.print("Transmitted: Left motor speed: ");
         Serial.print(tData.leftMotorSpeed);
         Serial.print(", right motor speed: ");
-        Serial.print(tData.rightMotorSpeed);
-        Serial.print(", reversed: ");
-        Serial.println(tData.reverse);
-        /*printn("sisisb",
-            "Transmitted: Left motor speed: ",
-            tData.leftMotorSpeed,
-            ", right motor speed: ",
-            tData.rightMotorSpeed,
-            ", reversed: ",
-            tData.reverse
-        );*/
-    }
-    else {
+        Serial.println(tData.rightMotorSpeed);
+    } else {
         Serial.println("Failed to transmit");
     }
-
-    delay(500);
 }
--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -21,14 +21,20 @@ const byte MOTOR_RIGHT_SPEED = 3;
 // ESC pin
 const byte ESC_SIG_PIN = 5;
 
+// Ultrasonic pins
+const byte FRONT_TRIG_PIN = A0;
+const byte FRONT_ECHO_PIN = A1;
+const byte LEFT_TRIG_PIN = A2;
+const byte LEFT_ECHO_PIN = A3;
+const byte RIGHT_TRIG_PIN = A4;
+const byte RIGHT_ECHO_PIN = A5;
 
 struct TransData {
     int leftMotorSpeed;
     int rightMotorSpeed;
-    bool reverse;
     bool tV = false;
 };
-
+const int DataSize = sizeof(TransData);
 
 // Defining transceiver things
 RF24 transceiver(CE_PIN, CSN_PIN);
@@ -39,10 +45,17 @@ const byte writingPipe[6] = "000001";
 Servo ESC;
 
 // Must be floats for correct decimals when printing
-const float ESC_ARMING_VAL = 50;
-const float ESC_MAX_VAL = 179;
+const float ESC_ARMING_VAL = 1060;
+const float ESC_MAX_VAL = 1860;
 const float ESC_RANGE = ESC_MAX_VAL - ESC_ARMING_VAL;
 
+long front_dist = 0;
+long left_dist = 0;
+long right_dist = 0;
+
+bool autoVac = false;
+
+bool isNegative(int n) { return n < 0; }
 
 void setup() {
     // Begin serial (we don't wait because if it's not connect to a computer, it will wait forever)
@@ -51,15 +64,16 @@ void setup() {
     // Transceiver setup
     if (!transceiver.begin()) {
         Serial.println(F("Transceiver hardware not responding"));
-        while (1) { }  // Hold program in infinite loop until transceiver is ready
+        while (1) {} // Hold program in infinite loop until transceiver is ready
     }
     transceiver.openReadingPipe(0, readingPipe);
     transceiver.setPALevel(RF24_PA_MIN);
     transceiver.startListening();
+    transceiver.setPayloadSize(DataSize);
 
     // ESC setup
     ESC.attach(ESC_SIG_PIN);
-    ESC.write(ESC_ARMING_VAL);
+    ESC.writeMicroseconds(ESC_ARMING_VAL);
 
     // Set up motor pins
     // Left
@@ -71,85 +85,137 @@ void setup() {
     pinMode(MOTOR_RIGHT_IN_2, OUTPUT);
     pinMode(MOTOR_RIGHT_SPEED, OUTPUT);
 
-    // Set up motor directions and initial speeds
+    // Set up ultrasonic pins
+    pinMode(FRONT_TRIG_PIN, OUTPUT);
+    pinMode(FRONT_ECHO_PIN, INPUT);
+    pinMode(LEFT_TRIG_PIN, OUTPUT);
+    pinMode(LEFT_ECHO_PIN, INPUT);
+    pinMode(RIGHT_TRIG_PIN, OUTPUT);
+    pinMode(RIGHT_ECHO_PIN, INPUT);
+
+    // Set initial speeds
     // Left
-    digitalWrite(MOTOR_LEFT_IN_1, LOW);
-    digitalWrite(MOTOR_LEFT_IN_2, HIGH);
     analogWrite(MOTOR_LEFT_SPEED, 0);
     // Right
-    digitalWrite(MOTOR_RIGHT_IN_1, LOW);
-    digitalWrite(MOTOR_RIGHT_IN_2, HIGH);
     analogWrite(MOTOR_RIGHT_SPEED, 0);
 }
 
 void loop() {
-    if (transceiver.available()) {
-        TransData tData;
-        transceiver.read(&tData, sizeof(tData));
-        Serial.print(F("Received: Left motor speed: "));
-        Serial.print(tData.leftMotorSpeed);
-        Serial.print(F(", right motor speed: "));
-        Serial.print(tData.rightMotorSpeed);
-        Serial.print(F(", reversed: "));
-        Serial.println(tData.reverse);
+    if (Serial.available()) {
+        long n = Serial.parseInt();
+        if (n == 0)
+            return;
+        Serial.print("Motor set to ");
+        Serial.println(n);
+        stopMotors();
+        ESC.writeMicroseconds(1060 + n);
+    }
 
-        setMotorDirection(tData.reverse);
+    if (autoVac) {
+        getDistancesFromSonics();
 
-        if (tData.leftMotorSpeed == 0 && tData.rightMotorSpeed == 0)
-            stopMotors();
-        else
-            setMotorSpeed(tData.leftMotorSpeed, tData.rightMotorSpeed);
+        Serial.print("Front: ");
+        Serial.print(front_dist);
+        Serial.print(", Left: ");
+        Serial.print(left_dist);
+        Serial.print(", Right: ");
+        Serial.println(right_dist);
 
+        // Facing wall head on
+        if (front_dist < 16) {
+            // Right has more space 
+            if (left_dist < right_dist)
+                setMotors(255, -255);
+            // Left has more space
+            else if (left_dist > right_dist)
+                setMotors(-255, 255);
+            // Neither has more space
+            else
+                setMotors(-255, -255);
+        } else
+            setMotors(255, 255);
+    } else {
+        if (transceiver.available()) {
+            TransData tData;
+            transceiver.read( & tData, DataSize);
+            Serial.print(F("Received: Left motor speed: "));
+            Serial.print(tData.leftMotorSpeed);
+            Serial.print(F(", right motor speed: "));
+            Serial.println(tData.rightMotorSpeed);
 
-        // Experiment
-        if (tData.tV) {
-            Serial.println("Exp trig received");
-            stopMotors();
-            activateVac();
+            setMotors(tData.leftMotorSpeed, tData.rightMotorSpeed);
+
+            // Experiment
+            if (tData.tV) {
+                return;
+                Serial.println("Exp trig received");
+                Serial.read();
+                stopMotors();
+                activateVac();
+            }
         }
     }
 }
 
+void setMotors(int left, int right) {
+    // Set direction
+    // Left
+    digitalWrite(MOTOR_LEFT_IN_1, isNegative(left) ? HIGH : LOW);
+    digitalWrite(MOTOR_LEFT_IN_2, isNegative(left) ? LOW : HIGH);
+    // Right
+    digitalWrite(MOTOR_RIGHT_IN_1, isNegative(right) ? HIGH : LOW);
+    digitalWrite(MOTOR_RIGHT_IN_2, isNegative(right) ? LOW : HIGH);
 
-void setMotorSpeed(int left, int right) {
-    analogWrite(MOTOR_LEFT_SPEED, left);
-    analogWrite(MOTOR_RIGHT_SPEED, right);
+    // Set speed to the absolute of their value
+    analogWrite(MOTOR_LEFT_SPEED, abs(left));
+    analogWrite(MOTOR_RIGHT_SPEED, abs(right));
 }
 
-// An alias for setMotorSpeed(0, 0)
-void stopMotors() { setMotorSpeed(0, 0); }
-
-// This prevents rapid writing of digital values when not nessesary
-bool dirRevFlag = false;
-void setMotorDirection(bool reversed) {
-    if (dirRevFlag != reversed) {
-        if (reversed) {
-            // Left
-            digitalWrite(MOTOR_LEFT_IN_1, HIGH);
-            digitalWrite(MOTOR_LEFT_IN_2, LOW);
-            // Right
-            digitalWrite(MOTOR_RIGHT_IN_1, HIGH);
-            digitalWrite(MOTOR_RIGHT_IN_2, LOW);
-        }
-        else {
-            // Left
-            digitalWrite(MOTOR_LEFT_IN_1, LOW);
-            digitalWrite(MOTOR_LEFT_IN_2, HIGH);
-            // Right
-            digitalWrite(MOTOR_RIGHT_IN_1, LOW);
-            digitalWrite(MOTOR_RIGHT_IN_2, HIGH);
-        }
-        dirRevFlag = reversed;
-    }
-}
-
+// An alias for setMotors(0, 0)
+void stopMotors() { setMotors(0, 0); }
 
 void activateVac() {
-    ESC.write(100);
-    delay(1000);
+    // Ramp up motor
+    for (int i = 0; i < 800; i++) {
+        ESC.writeMicroseconds(1060 + i);
+        delay(10);
+    }
+    delay(3000);
     deactivateVac();
 }
 
 void deactivateVac() {
-    ESC.write(ESC_ARMING_VAL);
+    ESC.writeMicroseconds(ESC_ARMING_VAL);
 }
+
+void getDistancesFromSonics() {
+    digitalWrite(FRONT_TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(FRONT_TRIG_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(FRONT_TRIG_PIN, LOW);
+    long front_duration = pulseIn(FRONT_ECHO_PIN, HIGH);
+
+    digitalWrite(LEFT_TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(LEFT_TRIG_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(LEFT_TRIG_PIN, LOW);
+    long left_duration = pulseIn(LEFT_ECHO_PIN, HIGH);
+
+    digitalWrite(RIGHT_TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(RIGHT_TRIG_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(RIGHT_TRIG_PIN, LOW);
+    long right_duration = pulseIn(RIGHT_ECHO_PIN, HIGH);
+
+    // You can't trigger them in parallel due to pulseIn
+    // And for some reason this now executes extrodinarily fast
+
+    front_dist = microsecondsToCentimetres(front_duration);
+    left_dist = microsecondsToCentimetres(left_duration);
+    right_dist = microsecondsToCentimetres(right_duration);
+}
+
+long microsecondsToCentimetres(long microseconds) { return microseconds / 29 / 2; }
